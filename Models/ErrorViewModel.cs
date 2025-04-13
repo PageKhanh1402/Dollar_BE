e896bb896c3f02d889a09186e31af719014025bf
Bảo Khánh Nguyễn <khanh1810jhg@gmail.com>
2025-04-13 22:30:48 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DollarProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
