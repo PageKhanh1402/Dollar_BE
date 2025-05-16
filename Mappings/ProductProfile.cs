2aa39579bf1b3a8e61e9d7cb1857e921803017e1
sdang <sdang6666@gmail.com>
2025-05-16 18:58:24 +0700

    market place

---
+++ Mappings/ProductProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DollarProject.Dto;
+using DollarProject.Models;
+
+namespace DollarProject.Mappings
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            //CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.CategoryName))
+                .ForMember(dest => dest.SellerName, opt => opt.MapFrom(src => src.Seller.FirstName + " " + src.Seller.LastName))
+                .ForMember(dest => dest.IsVerifiedSeller, opt => opt.MapFrom(src => src.Seller.IsVerifiedSeller));
+        }
+    }
+}
