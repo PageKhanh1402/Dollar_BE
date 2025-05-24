d8594c38ceab1bcd8d10641b28f16ce3da2af775
sdang <sdang6666@gmail.com>
2025-05-24 17:06:32 +0700

    update auth, add approve-reject-delete-view posts

--- Areas/Admin/Controllers/UsersController.cs
+++ Areas/Admin/Controllers/UsersController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DollarProject.DbConnection;
 using DollarProject.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DollarProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
